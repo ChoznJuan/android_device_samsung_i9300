2ab5e39dcd5eae8ace2bed85c147fcbeda3869b5
D. Juan Gray <djg5826@gmail.com>
2015-02-25 18:14:56 -0500

    UPDATE BOOT ANIMATION

--- eos.mk
+++ eos.mk
@@ -1,6 +1,9 @@
 # Release name
 PRODUCT_RELEASE_NAME := i9300
 
+# Boot animation
+TARGET_SCREEN_HEIGHT :=1280
+TARGET_SCREEN-WIDTH :=720
 
 # Inherit some common Euphoria stuff.
 $(call inherit-product, vendor/eos/config/common_full_phone.mk)
@@ -14,7 +17,3 @@ PRODUCT_NAME := eos_i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
-
-# Copy device specific prebuilt files.
-PRODUCT_COPY_FILES += \
-    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1280x768.zip:system/media/bootanimation.zip
